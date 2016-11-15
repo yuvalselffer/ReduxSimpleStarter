f15bda667f347a212c8e4e3aae3400940fd04ff5
yuvalselffer <yuval@selffer.com>
2016-11-15 15:11:38 +0200

    added routing using react-router

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        {this.props.children}
+      </div>
     );
   }
 }
+
+export default App;
---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>Home Sweet Home</div>
+    );
+  }
+}
+
+export default Home;
