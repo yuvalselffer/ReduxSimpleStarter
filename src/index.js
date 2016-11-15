702b7be5f00025c728d97a5ab8d953cdb204ede6
yuvalselffer <yuval@selffer.com>
2016-11-15 23:31:28 +0200

    added redux-thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Router, browserHistory } from 'react-router';
 import ReduxPromise from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import routes from './routes';
 
-const createStoreWithMiddleware = applyMiddleware(
-  ReduxPromise
-)(createStore);
+const middlewares = compose(
+  applyMiddleware(ReduxPromise),
+  applyMiddleware(ReduxThunk)
+);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={createStore(reducers, middlewares)}>
     <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container'));
